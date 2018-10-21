e7660680104f302c9a92e50893a8c610c2633f0b
Marina Rumyansev <aniramrum@gmail.com>
2018-10-21 18:17:42 +0300

    some refactoring to custom TitleCase pipe

--- src/app/title-casing.pipe.ts
+++ src/app/title-casing.pipe.ts
@@ -11,26 +11,36 @@ export class TitleCasePipe implements PipeTransform {
             return null;
         }
 
-        const prepositions = [
-            'of',
-            'the',
-            'and'
-        ];
-
         let enteredWords = value.split(' ');
         let text: string;
 
         for (let i = 0; i < enteredWords.length; i++) {
 
-            if (i > 0 && prepositions.includes(enteredWords[i].toLocaleLowerCase())) {
-                enteredWords[i] = enteredWords[i].toLowerCase();
+            let word = enteredWords[i];
+
+            if (i > 0 && this.isPreposition(word)) {
+                enteredWords[i] = word.toLowerCase();
             } else {
-                enteredWords[i] = enteredWords[i].substr(0, 1).toUpperCase() + enteredWords[i].substr(1).toLowerCase();
+                enteredWords[i] = this.toTitleCase(word);
             }
         }
 
         text = enteredWords.join(' ');
 
         return text;
+    }
+
+    private toTitleCase(word: string) {
+        return  word.substr(0, 1).toUpperCase() + word.substr(1).toLowerCase();
+    }
+
+    private isPreposition (word: string): boolean {
+        const prepositions = [
+            'of',
+            'the',
+            'and'
+        ];
+        return prepositions.includes(word.toLocaleLowerCase());
+
     }
 }
