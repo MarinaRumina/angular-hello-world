02db01cffc5658ad4674531c433a70b0edc88d1f
Marina Rumyansev <aniramrum@gmail.com>
2018-10-22 17:44:44 +0300

    adding input and output props to favorite component

--- src/app/favorite/favorite.component.ts
+++ src/app/favorite/favorite.component.ts
@@ -1,15 +1,27 @@
-import { Component} from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
 
 @Component({
   selector: 'app-favorite',
   templateUrl: './favorite.component.html',
   styleUrls: ['./favorite.component.css']
+  // not recommended way to declare input properties
+  // inputs: ['isFavorite']
 })
 export class FavoriteComponent {
 
-  isFavorite = true;
+  // another approach
+  // using decorator @Input() to mark fields or properties as input properties, do not forget to import from the library
+  // building reusable components is a good practice to use aliases (nic-names) to input fields and props. (in the parentheses)
+  @Input('is-favorite') isFavorite: boolean;
+  @Output() change = new EventEmitter(); // raising custom event from the component
 
   onClick() {
     this.isFavorite = !this.isFavorite;
+    this.change.emit();
   }
+
+  constructor() {}
+
+
 }
