c103de73c483cd7c78bc94ba6005423c339fe3c2
Marina Rumyansev <aniramrum@gmail.com>
2018-10-28 18:09:19 +0200

    Directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,26 +8,49 @@ import { FavoriteChangedEventArgs } from './favorite/favorite.component';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'hello-world';
 
-  courses = [1, 2];
+  courses = [
+    {id: 1, name: 'course 1'},
+    {id: 2, name: 'course 2'},
+    {id: 3, name: 'course 3'}
+  ];
 
-  post = {
-    postTitle: 'Title',
-    isFavorite: true
-  };
+  onAdd() {
+    this.courses.push({id: 4, name: 'course 4'});
+  }
 
-  tweet = {
-    body: '...',
-    isLiked: true,
-    likesCount: 5
-  };
+  onRemove(course) {
+    let index = this.courses.indexOf(course);
+    this.courses.splice(index, 1);
+  }
 
-  task = {
-    body: '...',
-    isFulfilled: true,
-    fulfillmentCount: 7
-  };
+  onChange(course) {
+    course.name = 'UPDATED';
+  }
+  // ------------------------------------------
+  // viewMode = 'map';
+
+// ------------------------------------------
+  // title = 'hello-world';
+
+  // courses = [1, 2];
+
+  // post = {
+  //   postTitle: 'Title',
+  //   isFavorite: true
+  // };
+
+  // tweet = {
+  //   body: '...',
+  //   isLiked: true,
+  //   likesCount: 5
+  // };
+
+  // task = {
+  //   body: '...',
+  //   isFulfilled: true,
+  //   fulfillmentCount: 7
+  // };
 
   // to receive a simple value as an output from calling event
   // onFavoriteChanged(isFavorite) {
